efb04f40bc083cf3f98ca7734defd389f5ad9f7a
Empanadas <juanfabri69@hotmail.com>
2025-07-18 09:39:42 -0300

    fix: hora graficos -3

--- utils/js/dateConvert.js
+++ utils/js/dateConvert.js
@@ -1,6 +1,6 @@
 const convertIsoToDate = async (isoString) => {
 	const fecha = new Date(isoString)
-
+	fecha.setHours(fecha.getHours() - 3)
 	const año = fecha.getFullYear()
 	const mes = String(fecha.getMonth() + 1).padStart(2, '0')
 	const dia = String(fecha.getDate()).padStart(2, '0')
